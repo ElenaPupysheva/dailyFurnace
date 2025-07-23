61c4f3c7401f1f7b85f3cacb1d5a8bea8f2efe2b
Alonso <nyankem@gmail.com>
2025-07-23 13:15:36 +0300

    Настройка фрагмента детализированной информации о печи

---
+++ app/src/main/java/media/ui/DetailedFragment.kt
@@ -0,0 +1,24 @@
+package media.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alonsonya.dailyfurnace.databinding.FragmentDetailedBinding
+
+
+class DetailedFragment : Fragment() {
+    private var _binding: FragmentDetailedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d("DetailedFragment", "onCreateView: Fragment Detailed создан!")
+        _binding = FragmentDetailedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
